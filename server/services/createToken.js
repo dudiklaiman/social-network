7b585687c892d14892706f34c04edc35a40cc09d
Dudi Klaiman <dudiklaiman@gmail.com>
2023-12-31 17:56:20 +0200

    modified jwt expiration time to 14days

--- server/services/createToken.js
+++ server/services/createToken.js
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const createToken = (userId) => {
-    const token = jwt.sign({_id: userId}, process.env.TOKEN_SECRET, { expiresIn:'600mins' })
+    const token = jwt.sign({_id: userId}, process.env.TOKEN_SECRET, { expiresIn:'14d' })
     return token;
 }
